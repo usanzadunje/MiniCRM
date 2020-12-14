0a9e5984684d19e062f35bc76175777f06f7e113
Dusan Djordjevic <dussann1997@live.com>
2020-12-14 09:25:49 +0100

    Dodatk env copy file vec podesen za buduce korisnike

---
+++ env-copy.js
@@ -0,0 +1,16 @@
+//Enter credentials that are required from you down below
+
+module.exports = {
+    mysql: {
+        host: '',
+        user: '',
+        password: '',
+        database: '',
+        port: 
+    },
+
+    email: {
+        user: '',
+        pass: ''
+    }
+}
